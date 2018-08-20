4ea5436140cefea74b0a97a6d8610614f02e2697
raweldon <raweldon@users.noreply.github.com>
2018-08-20 14:03:15 -0400

    full uncert accounting

--- angular_uncerts.py
+++ angular_uncerts.py
@@ -1,57 +1,62 @@
 '''
-    Systematic uncertainties from stilbene measurements at TUNL
-    Relative uncertainties are large for small scatter angles (as expected)
-    Numer to quote is the total angular uncertainty (sigma_tot)
+Full uncertainty accounting
+Split into sytematic and propagated uncertainties -- sytematic order of magnitude larger than prop
 '''
 import numpy as np
 
+# measurements
+#       BL 70                                                              BR 70
+angles = [70., 60., 50., 40., 30., 20., 20., 30., 40., 50., 60., 70.]
+dists = [66.4, 63.8, 63.2, 63.6, 64.9, 65.8, 66.0, 65.3, 63.4, 62.7, 64.3, 66.5]
+Ens = [11.325, 4.825]
+Ed = [8.209, 1.611] # from http://www.tunl.duke.edu/magnet.php
+
 # systematic uncertainties
-# position
+## position
 crys_beam_alignment = 1 # cm
 laser_range_finder_centered = 0.1 
 backing_det_pos = 0.2 # laser width = 4mm
 support_align = 1 # backing det support alignment
 det_interaction_location = np.sqrt(2.)/2. # corners of cube
-
 sigma_pos = np.sqrt(crys_beam_alignment**2 + laser_range_finder_centered**2 + backing_det_pos**2 + support_align**2 + det_interaction_location**2)
-print sigma_pos
 
-# angle
+## angle
 axes_marks = 1 # degree
 housing_mount = 1
-#       BL 70                                                              BR 70
-angles = [70., 60., 50., 40., 30., 20., 20., 30., 40., 50., 60., 70.]
-dists = [66.4, 63.8, 63.2, 63.6, 64.9, 65.8, 66.0, 65.3, 63.4, 62.7, 64.3, 66.5]
-det_size = [np.rad2deg(np.arctan((2.54+sigma_pos)/d)) for d in dists]
- 
-sigma_ang = [np.sqrt(axes_marks**2 + housing_mount**2 + d**2) for d in det_size]
-sigma_ang2 = np.sqrt(axes_marks**2 + housing_mount**2)
+sigma_ang = np.sqrt(axes_marks**2 + housing_mount**2)
 
 
-# total uncert
-En_11 = 11.325 # MeV
-Ep_11 = [En_11 * np.sin(np.deg2rad(a))**2 for a in angles]
-Ep_11_uncert = [En_11 * np.sin(2*np.deg2rad(a)) * np.deg2rad(sigma_ang[i]) for i, a in enumerate(angles)]
+for n, En in enumerate(Ens):
+    print '\n----------------- En = ' + str(En) + ' -----------------'
+    print 'theta    sigma_sys    sigma_Ep_bar    sigma_tot'
+    ## systematics
+    sigma_sys_degree = [np.sqrt(np.arctan(sigma_pos/d)**2 + np.deg2rad(sigma_ang)**2) for d in dists]
+    sigma_sys = [En*np.sin(np.deg2rad(2*theta))*sigma_sys_degree[i] for i, theta in enumerate(angles)]
 
-En_4 = 4.825
-Ep_4 = [En_4 * np.sin(np.deg2rad(a))**2 for a in angles]
-Ep_4_uncert = [En_4 * np.sin(2*np.deg2rad(a)) * np.deg2rad(sigma_ang[i]) for i, a in enumerate(angles)]
+    # Propagated uncertainties (E_p = E_n * cos^2(theta))
+    ## uncert in neutron energy
+    md = 2.01410178    #D mass
+    mn = 1.008665      #neutron mass
+    m_he3 = 3.0160293  #He-3 mass
+    Q = 3.26891        #d(d,n) Q-value
+    sigma_Ed = 0.0005 # 500 eV (listed on tunl site)
+    d_En_d_Ed = (md*mn/(mn + m_he3)**2 + (m_he3 - md)/(mn + m_he3))/(2*np.sqrt((Ed[n]*(m_he3 - md) + m_he3*Q)/(mn + m_he3) + mn*md*Ed[n]/(mn + m_he3)**2)
+                ) + mn*md/(2*(mn + m_he3)*np.sqrt(mn*md*Ed[n]))  # from 8/20/2018 notes
 
-print [En_11 * np.sin(np.deg2rad(2*a)) * np.sqrt(np.tan(sigma_pos/dists[i])**2 + np.deg2rad(sigma_ang2)**2) for i, a in enumerate(angles)]
+    sigma_n_prod = 0.103/np.sqrt(3.) # 103 keV loss through d cell, uniform distribution
+    sigma_En = np.sqrt(sigma_Ed**2 + sigma_n_prod**2)
 
-print '   angle (deg) |  uncert (deg) | uncert 11.3 (MeV) | rel uncert | uncert 4.8 (MeV) | rel uncert'
-for i, sigma in enumerate(sigma_ang):
-    if i == 0:
-        print 'BL    ' + str(angles[i]) + '            ' + str(round(sigma,3)) + '            ' + str(round(Ep_11_uncert[i],3)
-               ) + '           ' + str(round(Ep_11_uncert[i]/Ep_11[i],3)) + '           ' + str(round(Ep_4_uncert[i],3)
-               ) + '           ' + str(round(Ep_4_uncert[i]/Ep_4[i],3))
-    elif i == len(sigma_ang)-1:
-        print 'BR    ' + str(angles[i]) + '            ' + str(round(sigma,3)) + '            ' + str(round(Ep_11_uncert[i],3)
-               ) + '           ' + str(round(Ep_11_uncert[i]/Ep_11[i],3)) + '           ' + str(round(Ep_4_uncert[i],3)
-               ) + '           ' + str(round(Ep_4_uncert[i]/Ep_4[i],3))
-    else:
-        print '      ' + str(angles[i]) + '            ' + str(round(sigma,3)) + '            ' + str(round(Ep_11_uncert[i],3)
-               ) + '           ' + str(round(Ep_11_uncert[i]/Ep_11[i],3)) + '           ' + str(round(Ep_4_uncert[i],3)
-               ) + '           ' + str(round(Ep_4_uncert[i]/Ep_4[i],3))
+    ## uncert in scatter angle
+    sigma_theta = [np.arctan((2.54)/d) for d in dists]
+
+    sigma_Ep = [np.sqrt((np.sin(np.deg2rad(theta))**2*sigma_En)**2 + (En*np.sin(np.deg2rad(2*theta))*sigma_theta[i])**2) for i, theta in enumerate(angles)]
+    N = 100 # counts
+    sigma_Ep_bar = sigma_Ep/np.sqrt(N)
+
+    # total uncertainty
+    sigma_tot = [np.sqrt(sigma_sys[i]**2 + sigma_Ep_bar[i]**2) for i, sig in enumerate(sigma_sys)]
+    for s, sig in enumerate(sigma_tot):
+        print '{:^7} {:>8} {:>13} {:>14}'.format(angles[s], round(sigma_sys[s],3), round(sigma_Ep_bar[s],3), round(sigma_tot[s],3))
+
 
 

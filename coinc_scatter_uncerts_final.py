8e78b10d1baa5e142cb1fa2d51d9ae8da79f3e86
Robert Allen Weldon Jr <raweldon@wolftech.ad.ncsu.edu>
2019-08-09 07:47:05 -0400

    updated for lo_analysis.py

--- coinc_scatter_uncerts_final.py
+++ coinc_scatter_uncerts_final.py
@@ -3,18 +3,20 @@
 Original code on desktop at /raweldon/Research/TUNL/git_programs/stilbene_uncertainties and on github
 Full uncertainty accounting
 Split into sytematic and propagated uncertainties -- sytematic order of magnitude larger than prop
+8/6/2019 - Note: removed sigma_en_sys term (in ej_228_uncerts) because it is encapulated in detector positional uncertainty
+         - updated to work with plot_acp_lo_curves function in lo_analysis.py
 '''
 import numpy as np
 
 def Ed_to_En(md, mn, m3He, Q, Ed):
     return ((np.sqrt(md*mn*Ed)/(mn + m3He)) + np.sqrt((md*mn*Ed)/(mn + m3He)**2 + (Ed*(m3He - md) + m3He*Q)/(mn + m3He)))**2
 
-def calc_ep_uncerts(counts, beam_4MeV, print_unc):
+def calc_ep_uncerts(counts, angles, dists,  beam_11MeV, print_unc):
     # measurements
     #       BL 70                                                    BR 70
-    angles = [70., 60., 50., 40., 30., 20., 20., 30., 40., 50., 60., 70.]
-    dists = [66.4, 63.8, 63.2, 63.6, 64.9, 65.8, 66.0, 65.3, 63.4, 62.7, 64.3, 66.5]
-    if beam_4MeV:
+    #angles = [70., 60., 50., 40., 30., 20., 20., 30., 40., 50., 60., 70.]
+    #dists = [66.4, 63.8, 63.2, 63.6, 64.9, 65.8, 66.0, 65.3, 63.4, 62.7, 64.3, 66.5]
+    if not beam_11MeV:
         En = 4.825
         Ed = 1.611
         Ed_loss_in_gas = 0.344
@@ -28,7 +30,6 @@ def calc_ep_uncerts(counts, beam_4MeV, print_unc):
     backing_det_pos = 0.3 # laser width = +-2mm, laser range finder center = +- 0.1, slop - rest
     support_align = 0.5 # backing det support alignment
     sigma_pos = np.sqrt(backing_det_pos**2 + support_align**2)# + det_interaction_location**2)
-    sigma_en_sys = 0.0397 # from pandas_ej228_plots.py (final paper plots)
 
     # constants
     md = 2.01410178    #D mass
@@ -45,13 +46,11 @@ def calc_ep_uncerts(counts, beam_4MeV, print_unc):
         print 'theta    sigma_sys    sigma_Ep    sigma_Ep_bar    rel_uncert    sigma_tot'
     ## systematics
     sigma_pos_degree = [np.arctan(sigma_pos/d) for d in dists]
-    sigma_theta_sys = [En*np.sin(np.deg2rad(2*theta))*sigma_pos_degree[i] for i, theta in enumerate(angles)]
-    sigma_sys = [np.sqrt(sigma_en_sys**2 + sig**2) for sig in sigma_theta_sys]
+    sigma_sys = [En*np.sin(np.deg2rad(2*theta))*sigma_pos_degree[i] for i, theta in enumerate(angles)]
 
     En_loss_in_gas = Ed_to_En(md, mn, m_he3, Q, Ed + Ed_loss_in_gas/2.) - Ed_to_En(md, mn, m_he3, Q, Ed - Ed_loss_in_gas/2)
 
     sigma_n_prod = En_loss_in_gas/np.sqrt(3.) # uniform distribution
-    #simga_n_prod_ed = Ed_loss_in_gas[n]/np.sqrt(3.)
 
     ## uncert in neutron energy
     sigma_En = np.sqrt(sigma_Ed**2 + sigma_n_prod**2)
@@ -62,7 +61,6 @@ def calc_ep_uncerts(counts, beam_4MeV, print_unc):
     sigma_Ep = [np.sqrt((np.sin(np.deg2rad(theta))**2*sigma_En)**2 + (En*np.sin(np.deg2rad(2*theta))*sigma_theta[i])**2)
                 for i, theta in enumerate(angles)]
     #print sigma_Ep
-    N = 100 # counts
     sigma_Ep_bar = [sigma_Ep[i]/np.sqrt(N) for i, N in enumerate(counts)]
 
     # total uncertainty
@@ -71,8 +69,12 @@ def calc_ep_uncerts(counts, beam_4MeV, print_unc):
         for s, sig in enumerate(sigma_tot):
             print '{:^7} {:>8} {:>11} {:>13} {:>14} {:>12}'.format(angles[s], round(sigma_sys[s],3), round(sigma_Ep[s],3), round(sigma_Ep_bar[s],3),
                                                                    round(sigma_Ep_bar[s]/Ep[s],3), round(sigma_tot[s],3))
-    return sigma_Ep_bar, sigma_tot
+    return sigma_tot
 
 if __name__ == '__main__':
-    calc_ep_uncerts([100]*12, beam_4MeV=True, print_unc=True)
-    calc_ep_uncerts([100]*12, beam_4MeV=False, print_unc=True)
+    # measurements
+    #       BL 70                                                    BR 70
+    angles = [70., 60., 50., 40., 30., 20., 20., 30., 40., 50., 60., 70.]
+    dists = [66.4, 63.8, 63.2, 63.6, 64.9, 65.8, 66.0, 65.3, 63.4, 62.7, 64.3, 66.5]
+    calc_ep_uncerts([100]*12, angles, dists, beam_11MeV=True, print_unc=True)
+    calc_ep_uncerts([100]*12, angles, dists, beam_11MeV=False, print_unc=True)
